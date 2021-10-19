a1854a3a09364d0927fe948bbb8f3a198be8a111
Joshman <ejembijoshman@gmail.com>
2021-10-19 14:11:35 +0100

    commit changes and updates

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # javascript-count-down-timer
+
+### chnages made to the count down timer app on 18th of October is a bright yellow background after timer count down ends.
+
+### Also an audio file begins to play the count down stops.
+
+### Onclick the reset button the audio music stops
--- index.js
+++ index.js
@@ -1,52 +1,61 @@
 // time left for one hour
 let timeLeft = 60;
 
+// audio file
+var audio = new Audio("Time up.mp3");
+
 // time function
 function time() {
-    let theTime = document.getElementById("display");
-    let mins = Math.floor(timeLeft / 60);
-    if (mins < 10){
-        mins = "0" + mins;
-    }
-    var secs = Math.floor(timeLeft % 60);
-    if (secs < 10){
-        secs = "0" + secs;
-    }
-    theTime.innerHTML = mins + ":" + secs;
-    timeLeft = timeLeft - 1;
+  let theTime = document.getElementById("display");
+  let mins = Math.floor(timeLeft / 60);
+  if (mins < 10) {
+    mins = "0" + mins;
+  }
+  var secs = Math.floor(timeLeft % 60);
+  if (secs < 10) {
+    secs = "0" + secs;
+  }
+  theTime.innerHTML = mins + ":" + secs;
+  timeLeft = timeLeft - 1;
 
-    // Notification
-    if(timeLeft === -1) {
-        clearInterval(countTime);
-        document.body.style.background= "yellow";
-    }
+  // Notification
+  if (timeLeft === -1) {
+    clearInterval(countTime);
+    document.body.style.background = "yellow";
+
+    // to play audio file after timer stops
+    audio.play();
+  }
 }
 
-countTime = setInterval(time, 1000)
+countTime = setInterval(time, 1000);
 
 // to start count down time
 function start() {
-    countTime = setInterval(time, 1000)
+  countTime = setInterval(time, 1000);
 }
 
 // to stop count down time
-function pause() {
-    clearInterval(countTime);
-
+function clear() {
+  clearInterval(countTime);
 }
 
 // to reset count down time
 function reset() {
-    timeLeft = 60;
-    document.body.style.background= "black"
+  timeLeft = 60;
+  document.body.style.background = "black";
+
+  // to stop audio file
+  audio.pause();
+  audio.currentTime = 0;
 }
 
 // to add more time
 function add() {
-    timeLeft = timeLeft + 30;
+  timeLeft = timeLeft + 30;
 }
 
 // to reduce time
 function minus() {
-    timeLeft = timeLeft < 30 ? timeLeft : timeLeft - 30;
+  timeLeft = timeLeft < 30 ? timeLeft : timeLeft - 30;
 }
