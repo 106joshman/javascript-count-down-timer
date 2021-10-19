670e115af78d44ce6c9720da3466ad088e649a6d
Joshman <ejembijoshman@gmail.com>
2021-10-19 13:33:05 +0100

    commit

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
 // time left for one hour
-let timeLeft = 3599;
+let timeLeft = 60;
 
 // time function
 function time() {
-    let time = document.getElementById("display");
+    let theTime = document.getElementById("display");
     let mins = Math.floor(timeLeft / 60);
-        if (mins < 10){
-            mins = "0" + mins;
-        }
-
+    if (mins < 10){
+        mins = "0" + mins;
+    }
     var secs = Math.floor(timeLeft % 60);
-        if (secs < 10){
-            secs = "0" + secs;
-        }
+    if (secs < 10){
+        secs = "0" + secs;
+    }
+    theTime.innerHTML = mins + ":" + secs;
+    timeLeft = timeLeft - 1;
 
-    time.innerHTML = mins + ":" + secs;
-    time = timeLeft -= 1;
+    // Notification
+    if(timeLeft === -1) {
+        clearInterval(countTime);
+        document.body.style.background= "yellow";
+    }
 }
 
 countTime = setInterval(time, 1000)
 
 // to start count down time
 function start() {
-    countTime = setInterval(time, 1000);
+    countTime = setInterval(time, 1000)
 }
 
 // to stop count down time
 function pause() {
     clearInterval(countTime);
+
 }
 
 // to reset count down time
 function reset() {
-    timeLeft = 3599;
+    timeLeft = 60;
+    document.body.style.background= "black"
 }
 
 // to add more time
@@ -42,12 +48,5 @@ function add() {
 
 // to reduce time
 function minus() {
-    timeLeft = timeLeft < 30? timeLeft : timeLeft - 30;
+    timeLeft = timeLeft < 30 ? timeLeft : timeLeft - 30;
 }
-
-// Notification
-// if ( timeLeft <= 0 ) {
-//     timeLeft = 0
-// } else {
-//     document.getElementById("display").innerHTML = "END"
-// }
